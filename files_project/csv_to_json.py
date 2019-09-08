7d99328e1551bff9f7c6156e78c06a05bd17e3ee
TanimaRanjan <tanimaranjan@gmail.com>
2019-09-08 17:22:15 -0400

    CSV read and write practice

---
+++ files_project/csv_to_json.py
@@ -0,0 +1,23 @@
+import json
+
+json_list = []
+csv_file = open('csv_file.txt', 'r')
+
+
+for line in csv_file.readlines():
+    club, city, country = line.strip().split(',')
+    data = {
+        'club': club,
+        'country': country,
+        'city': city
+    }
+
+    json_list.append(data)
+
+csv_file.close()
+
+json_file = open('json_file.txt', 'w')
+json.dump(json_list, json_file)
+json_file.close()
+
+
